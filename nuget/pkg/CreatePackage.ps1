c9ca6545a83be9d057595d58844ddbfaa30dffe5
BSick7 <brad.sickles@gmail.com>
2014-07-19 10:05:32 -0400

    Added distributables.  Updated version.

--- nuget/pkg/CreatePackage.ps1
+++ nuget/pkg/CreatePackage.ps1
@@ -1,8 +1,8 @@
 $libdestdir = $PWD.Path + "\content\lib\Fayde.Controls"
-copy ($PWD.Path + "\..\..\..\Source\Ex\Fayde.Controls.Ex\Fayde.Controls.Ex.js") ($libdestdir + "\Fayde.Controls.js")
-copy ($PWD.Path + "\..\..\..\Source\Ex\Fayde.Controls.Ex\Fayde.Controls.Ex.d.ts") ($libdestdir + "\Fayde.Controls.d.ts")
-copy ($PWD.Path + "\..\..\..\Source\Ex\Fayde.Controls.Ex\Themes\Default.theme.xml") ($libdestdir + "\Themes\")
-copy ($PWD.Path + "\..\..\..\Source\Ex\Fayde.Controls.Ex\Themes\Metro.theme.xml") ($libdestdir + "\Themes\")
+copy ($PWD.Path + "\..\..\Fayde.Controls.Ex.js") ($libdestdir + "\Fayde.Controls.js")
+copy ($PWD.Path + "\..\..\Fayde.Controls.Ex.d.ts") ($libdestdir + "\Fayde.Controls.d.ts")
+copy ($PWD.Path + "\..\..\Themes\Default.theme.xml") ($libdestdir + "\Themes\")
+copy ($PWD.Path + "\..\..\Themes\Metro.theme.xml") ($libdestdir + "\Themes\")
 
 $vpath = $PWD.Path + "\version.txt"
 $versionstring = Get-Content $vpath
@@ -10,10 +10,9 @@ $tokens = $versionstring.Split(".")
 
 $major = $tokens.Get(0)
 $minor = $tokens.Get(1)
-$build = $tokens.Get(2)
-$revision = ([int]$tokens.Get(3) + 1).ToString()
+$build = ([int]$tokens.Get(2) + 1).ToString()
 
-$newversion = "$major.$minor.$build.$revision"
+$newversion = "$major.$minor.$build"
 Set-Content -Value $newversion $vpath
 
 $specfile = $PWD.Path + "\Fayde.Controls.nuspec"
