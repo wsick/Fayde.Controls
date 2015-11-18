7d12d05d988793385f05a455d507941a42d8dd45
Brad Sickles <brad.sickles@gmail.com>
2015-11-18 16:53:54 -0500

    Fixing busyindicator test.

--- test/tests/busyindicator.ts
+++ test/tests/busyindicator.ts
@@ -1,16 +1,12 @@
 import BusyIndicator = Fayde.Controls.BusyIndicator;
 
-export function load () {
+export function load() {
     QUnit.module("BusyIndicator");
 
     QUnit.test("Set all", (assert) => {
         var bi = new BusyIndicator();
         bi.IsBusy = true;
-        bi.IsIndeterminate = false;
-        bi.Value = 1;
-		bi.MaxValue = 100;
-		bi.MinValue = 0;
-		bi.BusyContent = "I am a content";
+        bi.BusyContent = "I am a content";
         ok(true);
     });
 }
